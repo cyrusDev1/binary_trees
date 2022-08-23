263dd4970af5a4ae9132e20c9f74124e72dec57c
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-08-23 09:52:41 +0100

    task 9: update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	else if (tree->left != NULL && tree->right != NULL)
-		return (2);
-	return (1);
+	left_height =  tree->left != NULL ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right != NULL ? binary_tree_height(tree->right) + 1 : 0;
+	if (left_height >= right_height)
+		return (left_height);
+	return (right_height);
 }
