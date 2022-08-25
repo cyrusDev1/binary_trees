2ea3ab9255382c5d50329829c467908bc29c1b02
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-08-25 23:03:36 +0100

    task 21

--- binary_trees.h
+++ binary_trees.h
@@ -49,7 +49,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void printCurrentLevel(const binary_tree_t *tree, void (*func)(int), int level);
-int binary_tree_is_complete(const binary_tree_t *tree)
+int binary_tree_is_complete(const binary_tree_t *tree);
 int complete(const binary_tree_t *tree, size_t index, size_t size);
 
 #endif
